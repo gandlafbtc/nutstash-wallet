5c23ec2b5d3e58e029823412c828b4b101385cf8
gandlaf21 <gandlaf@proton.me>
2023-01-15 07:53:55 +0100

    fix null check on error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <div class="h-screen w-screen flex items-center justify-center">
-<h1>{$page.status} {$page.error.message}</h1>
+<h1>{$page.status} {$page.error?.message}</h1>
 	{#if $page.status === 500}
 		An error occured. Maybe your history is corrupted.
 		<button on:click={resetHistory}> Reset History </button>
