6456e1af6b687b7b2f4acb3df01479523417a4ec
gandlaf21 <gandlaf@proton.me>
2023-01-15 06:50:10 +0100

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { page } from '$app/stores';
+	import { history } from '../stores/history';
+
+	const resetHistory = () => {
+		history.set([]);
+	};
+</script>
+
+<div class="h-screen w-screen flex items-center justify-center">
+<h1>{$page.status} {$page.error.message}</h1>
+	{#if $page.status === 500}
+		An error occured. Maybe your history is corrupted.
+		<button on:click={resetHistory}> Reset History </button>
+	{/if}
+</div>
