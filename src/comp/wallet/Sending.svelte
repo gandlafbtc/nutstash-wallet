8bcbc0ad4f6d478aa668584e57ada2e792f7ef2a
gandlaf21 <gandlaf@proton.me>
2023-02-24 06:48:54 +0100

    add support for sending to nip5 address

--- src/comp/wallet/Sending.svelte
+++ src/comp/wallet/Sending.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { CashuMint, CashuWallet, getDecodedProofs, getEncodedProofs } from '@gandlaf21/cashu-ts';
-	import type { Proof } from "@gandlaf21/cashu-ts";
+	import type { Proof } from '@gandlaf21/cashu-ts';
 	import { toast } from '../../stores/toasts';
 	import type { Mint } from '../../model/mint';
 	import { mints } from '../../stores/mints';
@@ -37,6 +37,7 @@
 	let isLoading = false;
 	let sendToNostrKey = '';
 	let hasBeenCopied = false;
+	let nostrSendLoading = false;
 
 	const send = async () => {
 		tokensForMint = getTokensForMint(mint, $token);
@@ -68,8 +69,7 @@
 			//remove all tokens that have been sent to mint from storage
 			token.update((state) => getTokenSubset(state, tokensToSend));
 			//add the tokens that are being sent to pending
-			pendingTokens.update((state) =>  [...send, ...state]);
-
+			pendingTokens.update((state) => [...send, ...state]);
 
 			//add newly minted tokens that have been returned as change
 			token.update((state) => [...state, ...returnChange]);
@@ -121,17 +121,34 @@
 	const getEncryptedContent = async (): Promise<string> => {
 		return $useExternalNostrKey
 			? // @ts-expect-error
-			  await window.nostr.nip04.encrypt(sendToNostrKey, encodedToken)
-			: await nostrTools.nip04.encrypt($nostrPrivKey, getConvertedPubKey(), encodedToken);
+			  await window.nostr.nip04.encrypt(await getConvertedPubKey(), encodedToken)
+			: await nostrTools.nip04.encrypt($nostrPrivKey, await getConvertedPubKey(), encodedToken);
 	};
-	const getConvertedPubKey = () => {
-		return sendToNostrKey.startsWith('npub')?nostrTools.nip19.decode(sendToNostrKey).data:sendToNostrKey
-	}
+	const getConvertedPubKey = async () => {
+		await resolveNip05();
+
+		return sendToNostrKey.startsWith('npub')
+			? nostrTools.nip19.decode(sendToNostrKey).data
+			: sendToNostrKey;
+	};
+
+	const resolveNip05 = async () => {
+		console.log('test for nip-5');
+		if (!sendToNostrKey.includes('.')) {
+			return;
+		}
+		const profile = await nostrTools.nip05.queryProfile(sendToNostrKey);
+		if (profile?.pubkey) {
+			sendToNostrKey = profile?.pubkey;
+		}
+	};
+
 	const sendWithNostr = async () => {
 		try {
+			nostrSendLoading = true;
 			const event: Event = {
 				kind: nostrTools.Kind.EncryptedDirectMessage,
-				tags: [['p', getConvertedPubKey()]],
+				tags: [['p', await getConvertedPubKey()]],
 				content: await getEncryptedContent(),
 				created_at: Math.floor(Date.now() / 1000),
 				pubkey: await getPubKey()
@@ -158,6 +175,8 @@
 		} catch (e) {
 			console.error(e);
 			toast('error', 'The Token could not be sent over nostr', 'Error:');
+		} finally {
+			nostrSendLoading = false;
 		}
 	};
 
@@ -183,37 +202,41 @@
 				<p>Copy the new token and send it to to someone!</p>
 			</div>
 			<div class="flex gap-2">
-			<input
-				type="text"
-				class="w-full input input-primary"
-				id="send-token-input"
-				readonly
-				value={encodedToken}
-			/>
-			<button class="btn btn-square" on:click={copyToken}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
-					/>
-				</svg>
-			</button>
-		</div>
-
+				<input
+					type="text"
+					class="w-full input input-primary"
+					id="send-token-input"
+					readonly
+					value={encodedToken}
+				/>
+				<button class="btn btn-square" on:click={copyToken}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
+						/>
+					</svg>
+				</button>
+			</div>
 		</div>
 		<div class="pt-2 flex gap-2 items-center">
 			{#if $useNostr}
 				<p class="font-bold">Or send it to a Nostr pubkey:</p>
 				<div class="w-full flex items-center gap-2">
-					<input type="text" class="input input-primary" bind:value={sendToNostrKey} placeholder="npub / hex" />
+					<input
+						type="text"
+						class="input input-primary"
+						bind:value={sendToNostrKey}
+						placeholder="npub / hex"
+					/>
 				</div>
 			{/if}
 		</div>
@@ -221,26 +244,30 @@
 	<div class="flex gap-2">
 		<button class="btn {hasBeenCopied ? '' : 'btn-disabled'}" on:click={resetState}>ok</button>
 		{#if $useNostr}
-			<button
-				class="btn {sendToNostrKey ? 'btn-info' : 'btn-disabled'} flex gap-1"
-				on:click={sendWithNostr}
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6 rotate-12"
+			{#if nostrSendLoading}
+				<div class="btn btn-square btn-disabled loading" />
+			{:else}
+				<button
+					class="btn {sendToNostrKey ? 'btn-info' : 'btn-disabled'} flex gap-1"
+					on:click={sendWithNostr}
 				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-					/>
-				</svg>
-				Send over Nostr</button
-			>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6 rotate-12"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+						/>
+					</svg>
+					Send over Nostr</button
+				>
+			{/if}
 		{/if}
 	</div>
 {:else}
