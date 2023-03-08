bfcdda254780d8335dfeeac81f3f9c911bf60f0d
gandlaf21 <gandlaf@proton.me>
2023-03-08 10:52:03 +0100

    add scan for nostr keys

---
+++ src/comp/elements/ScanNpub.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { Html5QrcodeScanner } from 'html5-qrcode';
+	import { onDestroy, onMount } from 'svelte';
+
+	export let scannedNpub;
+    
+    let qrScanner: Html5QrcodeScanner
+
+    function onScanSuccess(decodedText, decodedResult) {
+		// handle the scanned code as you like, for example:
+		scannedNpub = decodedText;
+        if (browser) {
+           document.getElementById('html5-qrcode-button-camera-stop')?.click()
+
+		   let element = document.getElementById('npub-scan-modal');
+			element?.click()
+        }
+
+	}
+	function onScanFailure(error) {
+       
+	}
+
+    onDestroy(()=> {{
+        if (browser) {
+           document.getElementById('html5-qrcode-button-camera-stop')?.click()
+        }
+    }})
+
+	onMount(() => {
+		scannedNpub = false
+        qrScanner = new Html5QrcodeScanner(
+            'npub-qr-reader',
+            { fps: 10, qrbox: { width: 250, height: 250 } },
+            /* verbose= */ false
+        );
+		qrScanner.render(onScanSuccess, onScanFailure);
+	});
+	
+</script>
+
+<input type="checkbox" id="npub-scan-modal" class="modal-toggle" />
+
+<div class="modal">
+	<div class="modal-box">
+<div class="flex flex-col items-center h-full w-full gap-2">
+	<div class="flex justify-between w-full items-center gap-1">
+		<button class="btn-square btn">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+				/>
+			</svg>
+		</button>
+		<h2 class="font-bold text-xl">Scan a Lightning Invoice</h2>
+		<div class="btn-square invisible" />
+	</div>
+	<div class="w-full">
+		<div id="npub-qr-reader" class="w-full h-full" style="/qr-styles.css"/>
+	</div>
+</div>
+</div>
+</div>
+
+<style>
+    
+</style>
--- src/comp/wallet/Receiving.svelte
+++ src/comp/wallet/Receiving.svelte
@@ -10,6 +10,11 @@
 	import { history } from '../../stores/history';
 	import { HistoryItemType } from '../../model/historyItem';
 	import { getKeysetsOfTokens, validateMintKeys } from '../util/walletUtils';
+	import { QRCode } from 'svelte-qrcode-image/util';
+	import { QRCodeImage } from 'svelte-qrcode-image';
+	import { nostrPubKey, useExternalNostrKey, useNostr } from '../../stores/nostr';
+	import { getPublicKey, nip19 } from 'nostr-tools';
+	import { npubEncode } from 'nostr-tools/nip19';
 
 	export let active;
 
@@ -141,6 +146,28 @@
 			isLoadingMint = false;
 		}
 	};
+
+	const getEncodedNpub = async () => {
+		if (!$useNostr) {
+			return '';
+		}
+		if ($useExternalNostrKey) {
+			if (browser) {
+				const pubK = await window.nostr?.getPublicKey();
+				console.log(pubK)
+				if (!pubK) {
+					return ''
+				}
+				return nip19.npubEncode(pubK);
+			}
+			return '';
+		} else {
+			if (!$nostrPubKey) {
+				return ''
+			}
+			return Promise.resolve(nip19.npubEncode($nostrPubKey));
+		}
+	};
 </script>
 
 <div class="flex flex-col gap-2">
@@ -178,12 +205,11 @@
 				<label for="receive-token-input">
 					<p class="font-bold">Token:</p>
 				</label>
-				
+
 				<input
 					type="text"
 					class="w-full input input-primary"
 					id="receive-token-input"
-					
 					bind:value={encodedToken}
 					on:input={validateToken}
 				/>
@@ -214,10 +240,29 @@
 				{/if}
 			</div>
 		</div>
+		{#await getEncodedNpub() then npub}
+			{#if $useNostr && npub}
+				<!-- content here -->
+				<div class="flex items-center justify-center w-full">
+					<div class="flex flex-col gap-2 items-center">
+
+						<div class="badge badge-info gap-2">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+								<path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+							  </svg>
+							  Receive via nostr
+						</div>
+						<QRCodeImage text={npub} scale={6} displayType="canvas" />
+					</div>
+				</div>
+			{/if}
+		{/await}
 		<div class="flex  gap-2">
 			<button class="btn" on:click={resetState}>cancel</button>
 			<button class="btn {isValid ? 'btn-success' : 'btn-disabled'}" on:click={receive}
-				>receive</button
+				>
+				receive</button
 			>
 		</div>
 	{/if}
