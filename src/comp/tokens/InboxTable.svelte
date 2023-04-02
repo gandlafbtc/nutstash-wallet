b04fa1f355172d7862f371bfed83575f1f027e8e
gandlaf21 <gandlaf@proton.me>
2023-04-02 10:13:23 +0200

    fix nostr message gdi

--- src/comp/tokens/InboxTable.svelte
+++ src/comp/tokens/InboxTable.svelte
@@ -23,7 +23,7 @@
 		let hasError = 0;
 		isLoading = true;
 		for (const nM of $nostrMessages.filter((n) => !n.isAccepted)) {
-			if (!nM.token){
+			if (!nM.token.token){
 				return
 			}
 			const mint: CashuMint = new CashuMint(nM.token.token[0].mint);
