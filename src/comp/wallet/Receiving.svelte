478265319e237569af892f0ba72902e9aad68883
gandlaf21 <gandlaf@proton.me>
2023-07-10 14:57:21 +0900

    add qr to text area

--- src/comp/wallet/Receiving.svelte
+++ src/comp/wallet/Receiving.svelte
@@ -186,10 +186,27 @@
 			
 			<h2 class="font-bold text-2xl">Redeem Token</h2>
 
+			<div class="inline-block relative w-full">
+
 			<textarea
 			id="receive-token-input"
 			bind:value={encodedToken}
-			on:change={validateToken} class="textarea textarea-primary h-40" placeholder="Paste a cashu token. It should look like this: cashuAey...13a35efc"></textarea>
+			on:change={validateToken} class="textarea textarea-primary w-full h-40" placeholder="Paste a cashu token. It should look like this: cashuAey...13a35efc"></textarea>
+			<div class="absolute z-10 bottom-4 right-4">
+				<button class="" on:click={()=> {
+					activeR = 'scan-receive'
+					isToken = true
+					}}>
+					
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 013.75 9.375v-4.5zM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 01-1.125-1.125v-4.5zM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0113.5 9.375v-4.5z" />
+						<path stroke-linecap="round" stroke-linejoin="round" d="M6.75 6.75h.75v.75h-.75v-.75zM6.75 16.5h.75v.75h-.75v-.75zM16.5 6.75h.75v.75h-.75v-.75zM13.5 13.5h.75v.75h-.75v-.75zM13.5 19.5h.75v.75h-.75v-.75zM19.5 13.5h.75v.75h-.75v-.75zM19.5 19.5h.75v.75h-.75v-.75zM16.5 16.5h.75v.75h-.75v-.75z" />
+					  </svg>
+					  
+				</button>
+			</div>
+		</div>
+
 			{#if mintToAdd}
 				<div>
 					{mintToAdd}
@@ -213,25 +230,17 @@
 						{mintId ? mintId : ''}
 					</p>
 				{/if}
+				<div class="flex  gap-2 mt-10">
+					<button class="btn {isValid ? 'btn-success' : 'btn-disabled'}" on:click={receive}>
+						receive</button
+					>
+				</div>
 			</div>
 		</div>
 		
-		<div class="flex items-center justify-center">
-			<button class="btn btn-primary" on:click={()=> {
-				activeR = 'scan-receive'
-				isToken = true
-				}}>
-				
-				Scan Token
-			</button>
-		</div>
 		
 
-		<div class="flex  gap-2 mt-10">
-			<button class="btn {isValid ? 'btn-success' : 'btn-disabled'}" on:click={receive}>
-				receive</button
-			>
-		</div>
+
 	{/if}
 </div>
 {:else if activeR==='scan-receive'}	
