cc17a9706f528dc1350f11f058a7b03a037fb7ab
gandlaf21 <gandlaf@proton.me>
2023-07-26 13:32:42 +0700

    dropdown arrow on mint

--- src/comp/elements/MintSelector.svelte
+++ src/comp/elements/MintSelector.svelte
@@ -37,6 +37,12 @@ return `${first10}...${last10}`
 
 				{formatMintText(mint.mintURL)}
 			</p>
+			<div>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+				  </svg>
+				  
+			</div>
 		</div>
 	</label>
 
