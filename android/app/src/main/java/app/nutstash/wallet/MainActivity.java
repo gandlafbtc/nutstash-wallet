6ddbcd0abbf6ff5182a4779c91c814a8446d2b27
gandlaf21 <gandlaf@proton.me>
2024-05-16 16:16:05 +0900

    add capacitator

---
+++ android/app/src/main/java/app/nutstash/wallet/MainActivity.java
@@ -0,0 +1,5 @@
+package app.nutstash.wallet;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.nutstash.wallet',
+  appName: 'nutstash',
+  webDir: 'build'
+};
+
+export default config;
