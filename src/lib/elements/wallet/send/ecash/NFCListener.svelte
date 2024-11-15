abc7ad8913ab8380f8bc2262950518a44377f879
gandlaf21 <gandlaf@proton.me>
2024-11-15 18:01:43 +0900

    try without abort controller

--- src/lib/elements/wallet/send/ecash/NFCListener.svelte
+++ src/lib/elements/wallet/send/ecash/NFCListener.svelte
@@ -3,51 +3,49 @@
     import { toast } from "svelte-sonner";
     import { push } from "svelte-spa-router";
 
+    let scanned = $state("");
 
-    let scanned = $state('')
-
-    onMount(()=> {
-        const abortController = new AbortController();
-            const ndef = new NDEFReader();
-            ndef.scan({signal: abortController}).then(() => {
-            toast.info('Scanning for NFC tag')
-            ndef.onreadingerror = () => {
-                console.log("Cannot read data from the NFC tag. Try another one?");
-                toast.error('Error reading from tag')
-            };
-            ndef.onreading = event => {
-                scanned = ''
-                const message = event.message
-                for (const record of message.records) {
-                    if (record.recordType !== 'text') {
-                        continue
+    onMount(() => {
+        const ndef = new NDEFReader();
+        ndef.scan()
+            .then(() => {
+                toast.info("Scanning for NFC tag");
+                ndef.onreadingerror = () => {
+                    console.log(
+                        "Cannot read data from the NFC tag. Try another one?",
+                    );
+                    toast.error("Error reading from tag");
+                };
+                ndef.onreading = (event) => {
+                    scanned = "";
+                    const message = event.message;
+                    for (const record of message.records) {
+                        if (record.recordType !== "text") {
+                            continue;
+                        }
+                        const textDecoder = new TextDecoder(record.encoding);
+                        const decoded = textDecoder.decode(record.data);
+                        scanned = scanned + decoded;
                     }
-                    const textDecoder = new TextDecoder(record.encoding);
-                    const decoded = textDecoder.decode(record.data)
-                    scanned = scanned + decoded
-                }
-                if (scanned.startsWith('cashu')) {
-                    toast.info('Cashu token scanned')
-                    push('/wallet/receive/cashu/'+scanned)
-                }
-                else {
-                    toast.warning('Scanned tag was not a cashu token: ' + scanned)
-                }
-            };
-            }).catch(error => {
-            console.log(`Error! Scan failed to start: ${error}.`);
+                    if (scanned.startsWith("cashu")) {
+                        toast.info("Cashu token scanned");
+                        push("/wallet/receive/cashu/" + scanned);
+                    } else {
+                        toast.warning(
+                            "Scanned tag was not a cashu token: " + scanned,
+                        );
+                    }
+                };
             })
-        return ()=> {
-            
-            abortController.abort()
-        }
-
-    }) 
+            .catch((error) => {
+                console.log(`Error! Scan failed to start: ${error}.`);
+            });
+        return () => {
+        };
+    });
 </script>
 
 <div class="flex items-center justify-center">
-    <div class="relative bg-green-500 rounded-full w-1 h-1">
-    </div>
-    <div class="absolute bg-green-500 rounded-full w-2 h-2 animate-ping">
-    </div>
+    <div class="relative bg-green-500 rounded-full w-1 h-1"></div>
+    <div class="absolute bg-green-500 rounded-full w-2 h-2 animate-ping"></div>
 </div>
