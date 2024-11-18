7cf2f7c042e01af38fc82e4856b3e0e64cea0a9b
gandlaf21 <gandlaf@proton.me>
2024-11-18 22:53:45 +0900

    chat improvements

--- src/lib/elements/contacts/chat/Chat.svelte
+++ src/lib/elements/contacts/chat/Chat.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import {  sendNip17DirectMessageToNpub } from "$lib/actions/nostr";
+    import { sendNip17DirectMessageToNpub } from "$lib/actions/nostr";
     import Button from "$lib/components/ui/button/button.svelte";
     import ScrollArea from "$lib/components/ui/scroll-area/scroll-area.svelte";
     import Textarea from "$lib/components/ui/textarea/textarea.svelte";
@@ -15,76 +15,83 @@
     import { onMount } from "svelte";
     import MeMessageWrapper from "./MeMessageWrapper.svelte";
     import ThemMessageWrapper from "./ThemMessageWrapper.svelte";
-    import * as Dialog  from "$lib/components/ui/dialog";
+    import * as Dialog from "$lib/components/ui/dialog";
     import MintSelector from "$lib/elements/ui/MintSelector.svelte";
     import { mints } from "$lib/stores/persistent/mints";
     import SendEcash from "./SendEcash.svelte";
+    import * as Avatar from "$lib/components/ui/avatar";
 
+    let contact = $derived(getBy($contactsStore, $params?.npub ?? "", "npub"));
 
+    let scrollArea = $state();
 
-    let contact = $derived(getBy($contactsStore, $params?.npub??'', 'npub'))
+    let pubkey = $derived(nip19.decode(contact?.npub ?? "").data);
 
-    let scrollArea = $state()
+    let sortedMessages = $derived(
+        [...$messagesStore].sort((a, b) => b.created_at - a.created_at),
+    );
 
-    let pubkey = $derived(nip19.decode(contact?.npub??'').data)
-
-    let sortedMessages = $derived([...$messagesStore].sort((a, b) => a.created_at - b.created_at))
-
-    let chatText = $state('')
-
-
+    let chatText = $state("");
 
     let sendMessage = () => {
         if (!contact) {
-            toast('Select a contact')
-            return
+            toast("Select a contact");
+            return;
         }
-        sendNip17DirectMessageToNpub(contact.npub,  chatText)
-    }
+        sendNip17DirectMessageToNpub(contact.npub, chatText);
+    };
 
     let sendEcashToNpub = (tokenString: string) => {
         if (!contact) {
-            toast('Select a contact')
-            return
+            toast("Select a contact");
+            return;
         }
-        sendNip17DirectMessageToNpub(contact.npub, tokenString)
-    }
-
+        sendNip17DirectMessageToNpub(contact.npub, tokenString);
+    };
 </script>
+
 {#if contact}
-<div class="flex w-80">
-    <div class="top-20 fixed">
-        {contact.alias}
-    </div>
-    <div class="bottom-4 fixed felx flex-col gap-2 mx-2">
-            <ScrollArea class='h-96'>
+    <div class="flex flex-col items-start gap-3 w-80 h-full mt-32">
+        <div class="h-20 flex items-center justify-center gap-2">
+            <Avatar.Root>
+                <Avatar.Image src="" alt={contact.alias} />
+                <Avatar.Fallback
+                    >{contact.alias
+                        .split(" ")
+                        .reduce(
+                            (acc, cur) => acc + "" + cur[0]?.toUpperCase(),
+                            "",
+                        )}</Avatar.Fallback
+                >
+            </Avatar.Root>
+            {contact.alias}
+        </div>
+        <div class="h-screen">
+            <div class="flex flex-col-reverse overflow-auto h-2/3">
                 {#each sortedMessages as message}
-                    {#if message.tags[0][1]===pubkey}
-                     <MeMessageWrapper>
-                         <Message {message}></Message>
-                    </MeMessageWrapper>
-                    {:else if message.pubkey===pubkey }
-                     <ThemMessageWrapper>
-                    <Message alias={contact.alias} {message}></Message>
-                     </ThemMessageWrapper> 
+                    {#if message.tags[0][1] === pubkey}
+                        <MeMessageWrapper>
+                            <Message {message}></Message>
+                        </MeMessageWrapper>
+                    {:else if message.pubkey === pubkey}
+                        <ThemMessageWrapper>
+                            <Message alias={contact.alias} {message}></Message>
+                        </ThemMessageWrapper>
                     {/if}
                 {/each}
-            </ScrollArea>
-        <div class="w-80 flex gap-2">
-            <Textarea bind:value={chatText}></Textarea>
-            <div class="flex gap-2 flex-col">
-                <Button onclick={sendMessage}>
-                    <Send></Send>
-                </Button>
-                <SendEcash to={contact} sendCallback={sendEcashToNpub}></SendEcash>
+            </div>
+            <div class="w-80 flex gap-2">
+                <Textarea bind:value={chatText}></Textarea>
+                <div class="flex gap-2 flex-col">
+                    <Button onclick={sendMessage}>
+                        <Send></Send>
+                    </Button>
+                    <SendEcash to={contact} sendCallback={sendEcashToNpub}
+                    ></SendEcash>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
-
-
 {:else}
     Contact not found
 {/if}
-
--- src/lib/elements/contacts/chat/SendEcash.svelte
+++ src/lib/elements/contacts/chat/SendEcash.svelte
@@ -1,28 +1,39 @@
 <script lang="ts">
     import Button from "$lib/components/ui/button/button.svelte";
-    import { Banknote, Send } from "lucide-svelte";
+    import { Banknote, LoaderCircle, Send } from "lucide-svelte";
     import * as Dialog  from "$lib/components/ui/dialog";
     import MintSelector from "$lib/elements/ui/MintSelector.svelte";
     import { mints } from "$lib/stores/persistent/mints";
     import UnitSelector from "$lib/elements/ui/UnitSelector.svelte";
     import { unit } from "$lib/stores/persistent/settings";
     import Input from "$lib/components/ui/input/input.svelte";
-    import { formatAmount } from "$lib/util/walletUtils";
+    import { formatAmount, getAmountForTokenSet, getProofsOfMintUnit } from "$lib/util/walletUtils";
     import { sendEcash } from "$lib/actions/actions";
-    import { nip19 } from "nostr-tools";
     import { getEncodedTokenV4, type Token } from "@cashu/cashu-ts";
+    import { proofsStore } from "$lib/stores/persistent/proofs";
+    import { toast } from "svelte-sonner";
 
 
     let {sendCallback, to} = $props()
     let isLoading = $state(false)
     let mint = $state($mints[0])
     let currentUnit = $state($unit)
+    let balance = $derived(getProofsOfMintUnit(mint, $proofsStore, currentUnit))
     let amount = $state(0)
     let sendEcashOpen = $state(false);
 
 
     const send = async () => {
         try {
+            isLoading = true
+            if (amount>getAmountForTokenSet(balance)) {
+                toast.warning( 'Insufficient balance' )
+                return
+            }
+            if (amount < 1) {
+                toast.warning('Amount must be greater than 0')
+                return
+            }
             isLoading = true
             console.log('mint' ,mint)
             const {send} = await sendEcash(mint.url, amount, {unit: currentUnit, includeFees: false})
@@ -32,6 +43,7 @@
                 mint: mint.url
             }
             sendCallback(getEncodedTokenV4(token))
+            sendEcashOpen = false
         } catch (error) {
             console.error(error)
         }
@@ -50,17 +62,23 @@
         </Dialog.Description>
       </Dialog.Header>
       <MintSelector bind:mint={mint}></MintSelector>
-      <div class="flex gap-2">
-          <Input type='number' bind:value={amount}></Input>
+      <div class="flex gap-2 items-center">
+
           <UnitSelector bind:currentUnit={currentUnit} selectedMints={[mint]}></UnitSelector>
+            {formatAmount(getAmountForTokenSet(balance), currentUnit)} available
         </div>
+        <Input type='number' bind:value={amount}></Input>
         send {formatAmount(amount, currentUnit)} to {to.alias}
-      <Dialog.Footer>
+      <Dialog.Footer class='flex gap-2'>
           <Button variant="outline" onclick={()=> {sendEcashOpen=false}}>
               Cancel
           </Button>
-          <Button onclick={send}>
-              <Send></Send>
+          <Button onclick={send} disabled={isLoading}>
+            {#if isLoading}
+                <LoaderCircle class='animate-spin'></LoaderCircle>
+            {:else}
+            <Send></Send>
+            {/if}  
               Send
           </Button>
       </Dialog.Footer>
