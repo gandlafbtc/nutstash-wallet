88f50b58a6d1839fa27303ed604a4c40c0eef895
gandlaf21 <gandlaf@proton.me>
2024-11-21 22:34:10 +0900

    fix tauri release

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,8 +7,8 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_nfc::init())
-        .plugin(tauri_plugin_barcode_scanner::init())
+        // .plugin(tauri_plugin_nfc::init())
+        // .plugin(tauri_plugin_barcode_scanner::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
