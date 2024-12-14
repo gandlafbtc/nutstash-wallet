6c25752af8dbf99471ab586022778b658109eeef
gandlaf21 <gandlaf@proton.me>
2024-12-14 13:20:46 +0900

    update backup version

--- src/comp/elements/BackupButton.svelte
+++ src/comp/elements/BackupButton.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { mints } from '../../stores/mints';
 	import { token } from '../../stores/tokens';
+	import { version } from '../../stores/version';
 
 	function backupTokens() {
 		var dataStr =
 			'data:text/json;charset=utf-8,' +
-			encodeURIComponent(JSON.stringify({ proofs: $token, mints: $mints }));
+			encodeURIComponent(JSON.stringify({ proofs: $token, mints: $mints, backupVersion: 'nutstash-legacy' }));
 		var downloadAnchorNode = document.createElement('a');
 		downloadAnchorNode.setAttribute('href', dataStr);
 		downloadAnchorNode.setAttribute('download', 'nutstash_backup.json');
