7064ad2d38ccad1810a06e6bf8e76bfd6932097e
gandlaf21 <gandlaf@proton.me>
2025-02-03 15:31:51 +0900

    Info about ecash checking

--- src/lib/elements/settings/ProofChecker.svelte
+++ src/lib/elements/settings/ProofChecker.svelte
@@ -6,7 +6,7 @@
 
 <div class="flex flex-col gap-3 items-start">
 <p class="text-lg font-bold">
-    Check proofs
+    Check ecash proofs
 </p>
 <Tabs.Root value="pending" class="w-[400px]">
     <Tabs.List>
@@ -14,7 +14,14 @@
       <Tabs.Trigger value="active">Active</Tabs.Trigger>
     </Tabs.List>
     <Tabs.Content value="pending">
-      <CheckProofSelector proofs={$pendingProofsStore} type='pending'></CheckProofSelector>
+      <div class="flex flex-col gap-3">
+
+        <p class="">
+            Check If sent ecash has been claimed. Un-claimed ecash will get added back to the wallet.
+        </p>
+        <CheckProofSelector proofs={$pendingProofsStore} type='pending'></CheckProofSelector>
+    </div>
+        
     </Tabs.Content>
     <Tabs.Content value="active">
         <div class="flex flex-col gap-3">
