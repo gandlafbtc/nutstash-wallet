056c16f7e003dac0d34169262bde20aa19719def
gandlaf21 <gandlaf@proton.me>
2025-02-26 14:57:58 +0900

    format

--- src/lib/elements/ui/QRCode.svelte
+++ src/lib/elements/ui/QRCode.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
-	import { encodeQR } from "@paulmillr/qr";
+	import { encodeQR } from '@paulmillr/qr';
 
 	interface Props {
-		data: string
+		data: string;
 	}
-	
+
 	let { data }: Props = $props();
 
-	let qrSVG = $derived(encodeQR(data, 'svg'))
-	
+	let qrSVG = $derived(encodeQR(data, 'svg'));
 </script>
 
-<div class="min-h-20 min-w-20 bg-white p-1 rounded-md">
+<div class="min-h-20 min-w-20 rounded-md bg-white p-1">
 	{@html qrSVG}
 </div>
